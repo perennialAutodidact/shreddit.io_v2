6ae9da32ba20ab15113fe791dc013032a8dd830e
Keegan Good <perennialautodidact@gmail.com>
2022-10-26 19:29:41 -0700

    moved all music theory types into own module

--- src/ts/stringedInstrument.ts
+++ src/ts/stringedInstrument.ts
@@ -1,40 +1,34 @@
-export type NoteName =
-  | "C"
-  | "C#"
-  | "Db"
-  | "D"
-  | "D#"
-  | "Eb"
-  | "E"
-  | "E#"
-  | "F"
-  | "F#"
-  | "Gb"
-  | "G"
-  | "G#"
-  | "Ab"
-  | "A"
-  | "A#"
-  | "Bb"
-  | "B"
-  | "Cb"
-  | "B#";
+import { NoteName, ScaleName, Interval, Note } from "./musicTheory";
 
-export type GuitarTuning = "standard" | "drop-d" | "open-d";
-export type MandolinTuning = "standard";
-export type UkuleleType = "standard";
+export type GuitarTuningNames = "standard" | "drop-d" | "open-d";
+export type MandolinTuningNames = "standard";
+export type UkuleleTypeNames = "standard";
+
 export interface Fret {
   noteName: NoteName;
 }
 
-export type StringedInstrumentName = "guitar" | "bass" | "mandolin" | "ukulele";
+export type StringedInstrumentName =
+  | "guitar"
+  | "bass"
+  | "mandolin"
+  | "ukulele"
+  | "piano";
 
 export interface StringData {
   rootNote: NoteName;
   frets: Fret[];
 }
+
+type Tuning = Note[];
+
 export interface StringedInstrumentState {
   instrumentType: StringedInstrumentName;
-  strings: StringData[];
-  tuning: "";
+  tuning: GuitarTuningNames | MandolinTuningNames | UkuleleTypeNames;
+  currentKey: NoteName;
+  scale: {
+    name: ScaleName;
+    intervals: Interval[];
+    notes: Note[];
+  };
 }
