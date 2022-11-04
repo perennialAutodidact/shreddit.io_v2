b42e37623bd16e05a2d7dcd669244714393be09a
Keegan Good <perennialautodidact@gmail.com>
2022-11-04 12:06:34 -0700

    updated types

--- src/ts/stringedInstrument.ts
+++ src/ts/stringedInstrument.ts
@@ -2,10 +2,38 @@ import { NoteName, ScaleName, Interval, Note } from "./musicTheory";
 
 export type GuitarTuningNames = "standard" | "drop-d" | "open-d";
 export type MandolinTuningNames = "standard";
-export type UkuleleTypeNames = "standard";
+export type UkuleleTuningNames = "standard";
 
-export interface Fret {
+export type Tunings = {
+  guitar: {
+    standard: ["e2", "a3", "d3", "g3", "b4", "e4"];
+    "drop-d": ["d2", "a3", "d3", "g3", "b4", "e4"];
+    dadgad: ["d2", "a3", "d3", "g3", "a4", "d4"];
+    "open-d": ["d2", "a3", "d3", "f#3", "a4", "d4"];
+  };
+  mandolin: {
+    standard: ["g2", "d3", "a3", "e4"];
+  };
+  ukulele: {
+    standard: ["g4", "c4", "e4", "a4"];
+  };
+};
+
+export interface FretData {
   noteName: NoteName;
+  octave: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
+}
+
+export function isNote(value: unknown): value is Note {
+  let _isNote = true
+  if (typeof value !== "string") {
+    return false;
+  } else if(value.length < 2){
+    return false;
+  } else {
+    if(value.slice(0,-1))
+    
+  }
 }
 
 export type StringedInstrumentName =
@@ -17,15 +45,17 @@ export type StringedInstrumentName =
 
 export interface StringData {
   rootNote: NoteName;
-  frets: Fret[];
+  frets: FretData[];
 }
 
-type Tuning = Note[];
+export type Tuning = Note[];
 
 export interface StringedInstrumentState {
   instrumentType: StringedInstrumentName;
-  tuning: GuitarTuningNames | MandolinTuningNames | UkuleleTypeNames;
+  tuningName: GuitarTuningNames | MandolinTuningNames | UkuleleTuningNames;
+  strings: Tuning;
   currentKey: NoteName;
+  neckLength: 12;
   scale: {
     name: ScaleName;
     intervals: Interval[];
