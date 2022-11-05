bc6cc30de6bdd8f9067c3c98bfe30f9ab640a3ad
Keegan Good <perennialautodidact@gmail.com>
2022-11-04 21:49:53 -0700

    reorganized common components and contants

--- src/common/constants/musicTheory.ts
+++ src/common/constants/musicTheory.ts
@@ -24,20 +24,3 @@ export const intervalsWithAug4: Interval[] = allIntervals.filter(
 export const intervalsWithDim5: Interval[] = allIntervals.filter(
   (interval) => interval !== "A4"
 );
-
-export const tunings: Tunings = {
-  guitar: {
-    standard: ["e2", "a3", "d3", "g3", "b4", "e4"],
-    "drop-d": ["d2", "a3", "d3", "g3", "b4", "e4"],
-    dadgad: ["d2", "a3", "d3", "g3", "a4", "d4"],
-    "open-d": ["d2", "a3", "d3", "f#3", "a4", "d4"],
-  },
-  mandolin: {
-    standard: ["g2", "d3", "a3", "e4"],
-  },
-  ukulele: {
-    standard: ["g4", "c4", "e4", "a4"],
-  },
-};
-
-export const MIN_NECK_LENGTH = 6;
---
+++ src/common/constants/stringedInstruments.ts
@@ -0,0 +1,20 @@
+import { Tunings } from "ts/stringedInstrument";
+
+export const tunings: Tunings = {
+  guitar: {
+    standard: ["e2", "a3", "d3", "g3", "b4", "e4"],
+    "drop-d": ["d2", "a3", "d3", "g3", "b4", "e4"],
+    dadgad: ["d2", "a3", "d3", "g3", "a4", "d4"],
+    "open-d": ["d2", "a3", "d3", "f#3", "a4", "d4"],
+  },
+  mandolin: {
+    standard: ["g2", "d3", "a3", "e4"],
+  },
+  ukulele: {
+    standard: ["g4", "c4", "e4", "a4"],
+  },
+};
+
+export const MIN_NECK_LENGTH = 6;
+
+export const INLAY_FRETS = [3, 5, 7, 9, 12, 15, 17, 19];
