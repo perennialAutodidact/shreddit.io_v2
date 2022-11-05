f5c04c6d240b13a1aa4f147d60cbfd115ff9b1f2
Keegan Good <perennialautodidact@gmail.com>
2022-11-04 21:53:49 -0700

    updated import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "styles/App.scss";
 import StringedInstrument from "./components/StringedInstrument";
-import TeoriaProvider from "components/TeoriaProvider";
+import TeoriaProvider from "common/components/TeoriaProvider";
 
 function App() {
   return (
