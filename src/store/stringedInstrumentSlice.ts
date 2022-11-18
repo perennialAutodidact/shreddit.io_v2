1e0a61f080d650922225aaa982506aa83c137b40
Keegan Good <perennialautodidact@gmail.com>
2022-11-17 22:45:18 -0800

    added const initial state values

--- src/store/stringedInstrumentSlice.ts
+++ src/store/stringedInstrumentSlice.ts
@@ -6,23 +6,29 @@ import {
 } from "@reduxjs/toolkit";
 import { NoteName, ScaleName } from "ts/musicTheory";
 import {
+  GuitarTuningNames,
   StringedInstrumentDimensions,
+  StringedInstrumentName,
   StringedInstrumentState,
 } from "ts/stringedInstrument";
+import { Note } from "ts/musicTheory";
 import { tunings } from "common/constants/stringedInstruments";
 const teoria = require("teoria");
 
-const INITIAL_SCALE: ScaleName = "ionian";
+const INITIAL_SCALE: ScaleName = "flamenco";
+const INITIAL_INSTRUMENT: StringedInstrumentName = "mandolin";
+const INITIAL_TUNING: GuitarTuningNames = "standard";
+const INITIAL_KEY: Note = "c1";
 export const initialState: StringedInstrumentState = {
-  instrumentType: "guitar",
-  tuningName: "standard",
-  strings: tunings["guitar"]["standard"],
-  currentKey: "c1",
-  totalFrets: 6,
+  instrumentType: INITIAL_INSTRUMENT,
+  tuningName: INITIAL_TUNING,
+  strings: tunings[INITIAL_INSTRUMENT][INITIAL_TUNING],
+  currentKey: INITIAL_KEY,
+  totalFrets: 12,
   scale: {
     name: INITIAL_SCALE,
-    intervals: teoria.note("c").scale(INITIAL_SCALE).scale,
-    notes: teoria.note("c").scale(INITIAL_SCALE).simple(),
+    intervals: teoria.note(INITIAL_KEY).scale(INITIAL_SCALE).scale,
+    notes: teoria.note(INITIAL_KEY).scale(INITIAL_SCALE).simple(),
   },
   markedNotes: [],
   dimensions: {
