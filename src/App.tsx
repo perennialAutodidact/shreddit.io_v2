f266b7e1535ba203819cf78a5897361f6c335350
Keegan Good <perennialautodidact@gmail.com>
2022-11-20 22:17:19 -0800

    added scale explorer page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import "styles/App.scss";
-import StringedInstrument from "./components/StringedInstrument";
+import ScaleExplorerPage from "pages/ScaleExplorerPage";
 import TeoriaProvider from "common/components/TeoriaProvider";
-import SettingsPage from "common/components/SettingsPage";
+import SettingsPage from "pages/SettingsPage";
 
 function App() {
   return (
     <div className="App p-lg-5 overflow-hidden position-relative vh-100">
       <TeoriaProvider>
         <Routes>
-          <Route path="/" element={<StringedInstrument />} />
+          <Route path="/" element={<ScaleExplorerPage />} />
           <Route path="/settings" element={<SettingsPage />} />
         </Routes>
       </TeoriaProvider>
