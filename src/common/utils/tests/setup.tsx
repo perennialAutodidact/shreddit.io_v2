a531260eef311c2e1fecd52e0dfef46fa08ec7d5
Keegan Good <perennialautodidact@gmail.com>
2022-11-20 22:16:48 -0800

    added router to tests setup

--- src/common/utils/tests/setup.tsx
+++ src/common/utils/tests/setup.tsx
@@ -2,11 +2,7 @@ import React, { FC, ReactElement } from "react";
 import { queries, render, RenderOptions } from "@testing-library/react";
 import { RenderResult } from "@testing-library/react/types";
 import configureStore from "redux-mock-store";
-import {
-  RouterProvider,
-  createMemoryRouter,
-  MemoryRouter,
-} from "react-router-dom";
+import { MemoryRouter, MemoryRouterProps } from "react-router-dom";
 import type { Router } from "react-router-dom";
 import { ReactRouter } from "ts/router";
 import { Provider as ReduxProvider } from "react-redux";
@@ -29,20 +25,24 @@ interface ExtendedRenderOptions extends RenderOptions {
 }
 
 const routes = [{ path: "/", element: <StringedInstrument /> }];
-export const router = createMemoryRouter(routes, {
-  initialEntries: ["/", "/settings"],
-  initialIndex: 0,
-});
 
-const AllTheProviders = (store: Store, router: ReturnType<typeof Router>) => {
+export const defaultRouter: MemoryRouterProps = {
+  initialEntries: ["/", "/settings"],
+  initialIndex: 1,
+};
+
+const AllTheProviders = (
+  store: Store,
+  router: Partial<typeof MemoryRouter>
+) => {
   return ({ children }: { children?: React.ReactNode }) => (
-    <RouterProvider router={router}>
+    <MemoryRouter {...router}>
       <ReduxProvider store={store}>
         <BreakpointProvider>
           <TeoriaProvider>{children}</TeoriaProvider>
         </BreakpointProvider>
       </ReduxProvider>
-    </RouterProvider>
+    </MemoryRouter>
   );
 };
 
@@ -50,7 +50,7 @@ const customRender = (
   ui: ReactElement,
   {
     initialState,
-    router,
+    router = defaultRouter,
     store = configureStore<Partial<RootState>>([])(initialState),
     options,
   }: ExtendedRenderOptions = {
