fbc21c7fd0a5bab39579e9f86773ad8503c80260
Keegan Good <perennialautodidact@gmail.com>
2022-12-27 00:11:27 -0800

    renamed fret to enharmonic within the current scale if necessary

--- src/components/StringedInstrument/Fret/index.tsx
+++ src/components/StringedInstrument/Fret/index.tsx
@@ -2,7 +2,7 @@ import React, { useMemo, useRef, useEffect, useContext } from "react";
 import { useAppSelector } from "store/hooks";
 import { BreakpointContext } from "common/components/BreakpointProvider/context";
 import { BreakpointState } from "ts/breakpoints";
-import { Interval } from "ts/musicTheory";
+import { Interval, Note } from "ts/musicTheory";
 import { FretData, FretNumber, StringNumber } from "ts/stringedInstrument";
 import { INLAY_FRET_INDICES } from "common/constants/stringedInstruments";
 import NoteMarker from "components/NoteMarker";
@@ -23,9 +23,18 @@ const Fret = ({ stringNumber, fretNumber, noteName }: FretProps) => {
   const { currentKey, scale } = useAppSelector(
     (appState) => appState.musicTheory
   );
+  const { activePitch } = useAppSelector((appState) => appState.audioClient);
 
   const fretRef = useRef<HTMLDivElement>(null);
 
+  const enharmonics = useMemo(() => {
+    return teoria
+      .note(noteName)
+      .enharmonics()
+      .map((note: any) => note.name() + note.accidental());
+  }, [noteName]);
+
+  // determine the fret's interval relative to the current key
   const fretInterval = useMemo<Interval | null>(() => {
     let note1 = teoria.note(currentKey);
     let note2;
@@ -52,6 +61,19 @@ const Fret = ({ stringNumber, fretNumber, noteName }: FretProps) => {
     return interval;
   }, [currentKey, scale.notes, noteName]);
 
+  // rename the note to an enharmonic in the current scale if necessary
+  noteName = useMemo(
+    () =>
+      !scale.notes.includes(noteName)
+        ? enharmonics.filter((enharmonic: any) => {
+            console.log(enharmonic);
+            return scale.notes.includes(enharmonic);
+          })[0]
+        : noteName,
+    [fretInterval, noteName, enharmonics]
+  );
+
+  // show fret marker if fret interval is in the scale's intervals
   const showMarker = useMemo(
     () => fretInterval && scale.intervals.includes(fretInterval),
     [scale.intervals, fretInterval]
@@ -81,7 +103,13 @@ const Fret = ({ stringNumber, fretNumber, noteName }: FretProps) => {
   );
 
   // determine size of fret marker
-  const markerScalar = useMemo(() => 0.5, []);
+  const markerScalar = useMemo(
+    () =>
+      activePitch && activePitch.toString().toLowerCase().startsWith(noteName)
+        ? 0.66
+        : 0.5,
+    [activePitch, noteName]
+  );
   const markerSize = useMemo(
     () => fret.height * markerScalar,
     [fret.height, markerScalar]
@@ -118,7 +146,6 @@ const Fret = ({ stringNumber, fretNumber, noteName }: FretProps) => {
       ) : (
         ""
       )}
-      {/* <span>{fretInterval}</span> */}
     </div>
   );
 };
