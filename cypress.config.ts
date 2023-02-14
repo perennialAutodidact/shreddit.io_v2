518c25bff2b2d640f76b20b063155e89d54b0361
Keegan Good <perennialautodidact@gmail.com>
2023-02-13 21:39:12 -0800

    added configuration values

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   videoCompression: false,
+  video: false,
+  watchForFileChanges: true,
   component: {
+    specPattern: ["**/*.cy.tsx", "**/*.cy.ts"],
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
   },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
