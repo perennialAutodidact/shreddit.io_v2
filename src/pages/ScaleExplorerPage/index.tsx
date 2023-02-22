31719117ca22a115d38759695fa12f794d0ae3f8
Keegan Good <perennialautodidact@gmail.com>
2023-02-22 00:23:15 -0800

    removed audio controls

--- src/pages/ScaleExplorerPage/index.tsx
+++ src/pages/ScaleExplorerPage/index.tsx
@@ -3,41 +3,16 @@ import * as Tone from "tone";
 import { useAppSelector } from "store/hooks";
 import StringedInstrument from "components/StringedInstrument";
 import IntervalDisplay from "components/IntervalDisplay";
-import AudioControls from "common/components/AudioControls";
-import AudioClient from "common/services/AudioClient";
 
 const ScaleExplorerPage = () => {
   const { versionNumbers } = useAppSelector((appState) => appState.app);
-  const [audioClientLoaded, setAudioClientLoaded] = useState<boolean>(false);
-
-  const audioClient = useRef<AudioClient>();
-
-  useEffect(() => {
-    if (window.AudioBuffer && !audioClient.current) {
-      audioClient.current = new AudioClient({
-        Tone,
-        instrument: "salamander",
-        onLoad: () => {
-          setAudioClientLoaded(true);
-        },
-      });
-    }
-    return () => audioClient.current && audioClient.current.cleanup();
-  }, []);
-
   return (
     <div className="container position-relative">
       <div className="row mb-3 mb-lg-5">
         <StringedInstrument />
       </div>
-      <div className="row mb-3">
+      <div className="row mb-lg-3">
         <IntervalDisplay markerSize={35} />
-      </div>
-      <div className="row">
-        <AudioControls
-          audioClient={audioClient.current}
-          isLoaded={audioClientLoaded}
-        />
       </div>
       <div className="row text-muted">v{versionNumbers.scaleExplorer}</div>
     </div>
