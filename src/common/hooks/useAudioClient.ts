f88e4d41d32be459b74c4b4d4b330b9d8b9e926f
Keegan Good <perennialautodidact@gmail.com>
2023-02-22 00:21:28 -0800

    created useAudioClient hook

---
+++ src/common/hooks/useAudioClient.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState, RefObject } from "react";
+import * as Tone from "tone";
+import AudioClient from "common/services/AudioClient";
+
+export const useAudioClient = () => {
+  const [audioClientLoaded, setAudioClientLoaded] = useState<boolean>(false);
+
+  const audioClientRef = useRef<AudioClient>();
+
+  useEffect(() => {
+    if (window.AudioBuffer && !audioClientRef.current) {
+      audioClientRef.current = new AudioClient({
+        Tone,
+        instrument: "salamander",
+        onLoad: () => {
+          setAudioClientLoaded(true);
+        },
+      });
+    }
+    return () => audioClientRef.current && audioClientRef.current.cleanup();
+  }, []);
+
+  return {
+    audioClientLoaded,
+    audioClient: audioClientRef.current ? audioClientRef.current : null,
+  };
+};
