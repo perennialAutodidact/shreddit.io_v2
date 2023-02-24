6b5dbd3b17428c515bbdd0d9fab8d0c9d45677f5
Keegan Good <perennialautodidact@gmail.com>
2023-02-23 19:28:26 -0800

    moved settings menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import ScaleExplorerPage from "pages/ScaleExplorerPage";
 import TeoriaProvider from "common/components/TeoriaProvider";
 import UtilityBar from "components/UtilityBar";
 import SettingsMenuToggle from "components/UtilityBar/SettingsMenuToggle";
+import SettingsMenu from "common/components/SettingsMenu";
 
 function App() {
   return (
     <div className="App p-lg-3 overflow-hidden position-relative min-vh-100">
       <TeoriaProvider>
         <SettingsMenuToggle />
+        <SettingsMenu />
         <UtilityBar />
         <Routes>
           <Route path="/" element={<ScaleExplorerPage />} />
