95aafcc7ec5e67167dbb08265cf068a007ff780e
Keegan Good <perennialautodidact@gmail.com>
2023-04-27 07:46:33 -0700

    generated selector grid from object instead of array

--- src/components/NoteMarkerRangeSelector/index.tsx
+++ src/components/NoteMarkerRangeSelector/index.tsx
@@ -1,11 +1,6 @@
-import {
-  createRef,
-  RefObject,
-  useEffect,
-  useLayoutEffect,
-  useState,
-} from "react";
+import { createRef, RefObject, useEffect, useState } from "react";
 import { useAppSelector } from "store/hooks";
+import { FretNumber, StringNumber } from "ts/stringedInstrument";
 
 const NoteMarkerRangeSelector = () => {
   const { fretTotal, strings } = useAppSelector(
@@ -13,18 +8,49 @@ const NoteMarkerRangeSelector = () => {
   );
 
   const containerRef = createRef<HTMLDivElement>();
-  const [cellRefs, setCellRefs] = useState<
-    Array<RefObject<HTMLDivElement> | null>
-  >([]);
+
+  type GridKey = `${StringNumber}_${FretNumber}`;
+  type Grid = {
+    [key in GridKey]: Cell;
+  };
+  type Cell = {
+    string: StringNumber;
+    fret: FretNumber;
+    ref: RefObject<HTMLDivElement>;
+    enabled: boolean;
+  };
+
+  const generateGrid = (): Grid => {
+    const items = [...Array(strings.length)].flatMap((_, string) =>
+      [...Array(fretTotal + 1)].map((_, fret) => [
+        `${string}_${fret}`,
+        {
+          string,
+          fret,
+          enabled: true,
+        },
+      ])
+    );
+
+    const grid = Object.fromEntries(items);
+    return grid;
+  };
+
+  const [grid, setGrid] = useState<Grid>(generateGrid());
+
   const [cellSize, setCellSize] = useState<number>(15);
 
-  useEffect(() => {
-    setCellRefs((cellRefs) =>
-      Array(strings.length)
-        .fill(null)
-        .map((_, i) => cellRefs[i] || createRef())
-    );
-  }, [strings.length]);
+  const rows = [...Array(strings.length)];
+  const cols = [...Array(fretTotal)];
+
+  const toggleCell = (string: StringNumber, fret: FretNumber) => {
+    const key = `${string}_${fret}` as GridKey;
+    const cell = grid[key];
+    setGrid({ ...grid, [key]: { ...cell, enabled: !cell.enabled } });
+  };
+
+  const getCell = (string: StringNumber, fret: FretNumber): Cell =>
+    grid[`${string}_${fret}` as GridKey];
 
   useEffect(() => {
     if (containerRef.current) {
@@ -37,20 +63,36 @@ const NoteMarkerRangeSelector = () => {
     <div
       id="note-marker-range-selector"
       data-test-id="NoteMarkerRangeSelector"
-      className="container"
+      className="container mt-4"
       ref={containerRef}
     >
-      {strings.map((string, i) => (
-        <div className="d-flex justify-content-center" ref={cellRefs[i]}>
-          {[...Array(fretTotal)].map((column, j) => (
-            <div
-              data-test-id="MarkerEnabled"
-              className="bg-danger border"
-              style={{ height: `${cellSize}px`, width: `${cellSize}px` }}
-            ></div>
-          ))}
-        </div>
-      ))}
+      {rows.map((_, i) => {
+        const row = i as StringNumber;
+        return (
+          <div key={row} className="d-flex justify-content-center">
+            {cols.map((_, j) => {
+              const col = j as FretNumber;
+              const cell = getCell(row, col);
+              return (
+                <div
+                  key={col}
+                  data-test-id="MarkerEnabled"
+                  className={`
+                    cell_${
+                      cell.enabled
+                        ? "enabled bg-success"
+                        : "disabled bg-secondary"
+                    }
+                    border
+                  `}
+                  style={{ height: `${cellSize}px`, width: `${cellSize}px` }}
+                  onClick={() => toggleCell(row, col)}
+                ></div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
